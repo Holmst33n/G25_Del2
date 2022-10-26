7b99c12ce5a4fbb1f28989c202cc52e1a94744d4
s205793 <s205793@dtu.dk>
2022-10-26 12:21:24 +0200

    Added curly bracket to throwDice method

--- game/src/DieCup.java
+++ game/src/DieCup.java
@@ -8,18 +8,18 @@ public class DieCup {
         rollValue = 0;
     }
 
-    public static void throwDice();
+    public static void throwDice() {
         Die die1 = new Die();
         Die die2 = new Die();
-        Die1.roll();
-        Die2.roll();
-        this.rollValue = Die1.getFaceValue() + Die2.getFaceValue(); // we plus the to dies together
-
+        die1.roll();
+        die2.roll();
+        this.rollValue = die1.getFaceValue() + die2.getFaceValue(); // we plus the to dies together
+    }
     public int getRollValue() {
         return rollValue;
     } // We get the total rollvalue
 
 }
-}
+
 
 
