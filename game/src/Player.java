86ca5a4827ce1c3a540e81a7a98b1129efb0b333
Mikkel Nørgaard <mikkel.brunstedt@hotmail.dk>
2022-10-26 09:25:40 +0200

    Player created

---
+++ game/src/Player.java
@@ -0,0 +1,16 @@
+package game.src;
+
+public class Player {
+
+    private int balance = 0;
+
+    public Player() {}
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+}
