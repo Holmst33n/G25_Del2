38b855600b15d5c605688e5084f5b9a6dc971f9a
Mikkel Nørgaard <mikkel.brunstedt@hotmail.dk>
2022-10-26 09:57:37 +0200

    Added setTurnIndicator and getTurnIndicator methods

--- game/src/Player.java
+++ game/src/Player.java
@@ -3,8 +3,11 @@ package game.src;
 public class Player {
 
     private int balance = 0;
+    private boolean turnIndicator;
 
-    public Player() {}      //constructor
+    public Player() {       //constructor
+        turnIndicator = false;
+    }
 
     public int getBalance() {
         return balance;
@@ -12,5 +15,18 @@ public class Player {
 
     public void setBalance(int balance) {       //setter
         this.balance = balance;
+    }
+
+    public void setTurnIndicator(int a) {       //sets turnIndicator to either true or false by setting a to 1 or 0
+        if(a == 1) {
+            turnIndicator = true;
+        }
+        else if (a == 0) {
+            turnIndicator = false;
+        }
+    }
+
+    public boolean getTurnIndicator() {     //gets turnIndicator
+        return turnIndicator;
     }
 }
