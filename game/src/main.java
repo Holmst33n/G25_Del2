1a065e00d40c2dca3fe5b3bee3eac23beb2a9898
Mikkel Nørgaard <mikkel.brunstedt@hotmail.dk>
2022-10-27 12:13:17 +0200

    Added more comments

--- game/src/main.java
+++ game/src/main.java
@@ -11,20 +11,20 @@ public class main {
         Scanner buffer = new Scanner(System.in);    //creates a buffer used to press enter to play next turn
         String turn;
 
-        do{
+        do{         //this do while loop runs until a player hits 3000
             turn = buffer.nextLine();
             board.playTurn();
             if(player1.isTurnIndicator()){          //plays player1's turn
                 player1.setBalance(player1.getBalance() + board.getThrowResult());      //updates balance of player1
-                player1.setTurnIndicator(false);
+                player1.setTurnIndicator(false);        //removes turnIndicator from player1
                 System.out.println("Player1 has earned " + board.getThrowResult());
-                if(board.isExtraTurn()){
+                if(board.isExtraTurn()){        //checks if player1 has an extra turn
                     player1.setTurnIndicator(true);
                     System.out.println("Player1 has earned an extra turn!");
                     board.setExtraTurn(false);
                 }
                 else{
-                    player2.setTurnIndicator(true);
+                    player2.setTurnIndicator(true);     //hands over the turn to player2
                 }
                 System.out.println("Player1 balance: " + player1.getBalance());
             }
@@ -33,21 +33,21 @@ public class main {
             board.playTurn();
             if(player2.isTurnIndicator()){          //plays player2's turn
                 player2.setBalance(player2.getBalance() + board.getThrowResult());      //updates balance of player2
-                player2.setTurnIndicator(false);
+                player2.setTurnIndicator(false);        //removes turnIndicator from player2
                 System.out.println("Player2 has earned " + board.getThrowResult());
-                if(board.isExtraTurn()){
+                if(board.isExtraTurn()){        //checks if player2 has an extra turn
                     player2.setTurnIndicator(true);
                     System.out.println("Player2 has earned an extra turn!");
                     board.setExtraTurn(false);
                 }
                 else{
-                    player1.setTurnIndicator(true);
+                    player1.setTurnIndicator(true);     //hands over the turn to player1
                 }
                 System.out.println("Player2 balance: " + player2.getBalance());
             }
         }while(player1.getBalance() < 3000 && player2.getBalance() < 3000);
 
-        if(player1.getBalance() > 3000) System.out.println("Player1 has won!");
-        else if(player2.getBalance() > 3000) System.out.println("Player2 has won!");
+        if(player1.getBalance() > 3000) System.out.println("Player1 has won!");         //checks if player one has won
+        else if(player2.getBalance() > 3000) System.out.println("Player2 has won!");    //checks if player two has won
     }
 }
