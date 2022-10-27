1773e5975d4c80459b0d5143723402a7c3534736
Holmst33n <113360170+Holmst33n@users.noreply.github.com>
2022-10-27 14:11:22 +0200

    Added Account class

---
+++ game/src/Account.java
@@ -0,0 +1,63 @@
+//-----------------------------------------------------------------
+//  Parts of this code comes from Lewis/Loftus, Java software solutions
+//-----------------------------------------------------------------
+
+package game.scr;
+
+public class Account {
+    private int balance;
+
+    //-----------------------------------------------------------------
+    //  Sets up the account by defining its owner, account number,
+    //  and initial balance.
+    //-----------------------------------------------------------------
+    public Account () {
+        balance = 0;
+    }
+
+    //-----------------------------------------------------------------
+    //  Deposits the specified amount into the account. Returns the
+    //  new balance.
+    //-----------------------------------------------------------------
+    public double deposit (int amount) {
+
+        if (amount < 1){ // Guard condition to prevent negative deposits
+            amount = 0;
+            System.out.println("Error: Not possible to withdraw deposit negative founds");
+        }
+        balance = balance + amount;
+        return balance;
+    }
+
+    //-----------------------------------------------------------------
+    //  Withdraws the specified amount from the account and applies
+    //  the fee. Returns the new balance.
+    //-----------------------------------------------------------------
+    public double withdraw (int amount) {
+
+        if (amount < 1){ // Guard condition to prevent negative deposits
+            amount = 0;
+            System.out.println("Error: Not possible to withdraw negative founds");
+        }
+
+        if (amount > balance){ // Guard condition to prevent withdrawing an amount greater than the Account balance
+            amount = 0;
+            System.out.println("Error: Insufficient funds");
+        }
+
+        balance = balance - amount;
+        if (balance < 0) {
+            balance = 0;
+        }
+
+        return balance;
+    }
+
+    //-----------------------------------------------------------------
+    //  Returns the current balance of the account.
+    //-----------------------------------------------------------------
+    public double getBalance () {
+        return balance;
+    }
+
+}
